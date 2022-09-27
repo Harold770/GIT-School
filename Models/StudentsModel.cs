e3cbbac2bf2368947a219c29f5054372fc1c3e4e
Harold García <haroldsalatiel@gmail.com>
2022-09-27 11:23:43 -0500

    Creacion de modelo estudiante

---
+++ Models/StudentsModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GIT_School.Models
+{
+    public class StudentsModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        public string? Grade { get; set; }
+        [Required]
+        public string? Group { get; set; }
+        [Required]
+        public string? Age { get; set; }
+    }
+}
